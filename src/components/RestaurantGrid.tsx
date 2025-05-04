f27fae7caf6e0de3a52d637c8fae1a35b9924d9a
Scott Wambach <scott@scottwamba.ch>
2025-05-04 10:16:42 -0500

    Update RestaurantGrid.tsx

--- src/components/RestaurantGrid.tsx
+++ src/components/RestaurantGrid.tsx
@@ -15,7 +15,7 @@ export const RestaurantGrid = ({
       <h2 className="text-center uppercase text-pangea font-black tracking-widest pt-[80px] pb-[50px] text-[26px]">
         Select your destination
       </h2>
-      <div className="grid mobile-lg:grid-cols-2 gap-[30px] mobile-lg:grid-cols-2 tablet-xl:grid-cols-4">
+      <div className="grid mobile-lg:grid-cols-2 gap-[30px] tablet-xl:grid-cols-4">
         {items.map((item) => (
           <LocationCard key={item.name} {...item} />
         ))}
