473651cd8dfcb7704765181b876bb9cd4a83ff13
Scott Wambach <scott@scottwamba.ch>
2025-05-10 10:10:55 -0500

    text center fix

--- src/components/TasteMakers.tsx
+++ src/components/TasteMakers.tsx
@@ -5,7 +5,7 @@ import { MediaCard } from "./modules/MediaCard";
 
 export const TasteMakers = ({ items }: { items: TasteMakerItemProps[] }) => {
   return (
-    <section className="relative px-8 py-[100px] tablet:text-center">
+    <section className="relative px-8 py-[100px]">
       <Image
         src={tasteBg.src}
         width={tasteBg.width}
@@ -16,10 +16,10 @@ export const TasteMakers = ({ items }: { items: TasteMakerItemProps[] }) => {
         className="absolute top-0 left-0 w-full h-full object-cover -z-10 select-none pointer-events-none"
       />
       <div className="max-w-[1200px] mx-auto">
-        <h3 className="text-white uppercase font-black tracking-widest pb-[50px] text-[26px]">
+        <h3 className="text-white text-center uppercase font-black tracking-widest pb-[50px] text-[26px]">
           For our Tastemakers
         </h3>
-        <div className="grid tablet:grid-cols-2 tablet-xl:grid-cols-3 gap-8 desktop-lg:gap-[60px]">
+        <div className="grid tablet:grid-cols-2 tablet:text-center tablet-xl:grid-cols-3 gap-8 desktop-lg:gap-[60px]">
           {items.map((item) => (
             <MediaCard key={item._id} {...item} />
           ))}
