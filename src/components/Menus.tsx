683d7dc1683abc03d8bbd4aa17be2db407036512
Scott Wambach <scott@scottwamba.ch>
2025-05-11 13:58:06 -0500

    Update Menus.tsx

--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -19,11 +19,11 @@ export const Menus = ({
   return (
     <div className="flex flex-col gap-8 py-[100px] px-4 tablet-md:px-8">
       <SectionHeading title="Our Menus" className="text-pangea" element="h2" />
-      <div className="flex flex-col tablet-md:flex-row tablet-md:flex-wrap tablet-md:justify-center tablet-md:items-center w-full tablet-md:max-w-[1000px] tablet-md:mx-auto gap-4">
+      <div className="flex flex-col tablet-md:flex-row tablet-md:flex-wrap tablet-md:justify-center tablet-md:items-center w-full tablet-md:max-w-[1000px] tablet-md:mx-auto gap-8">
         {items.map((item) => (
           <button
             key={item.name}
-            className={`text-black uppercase tracking-widest text-[24px] tablet-md:w-[calc(33.3333%-16px)] cursor-pointer hover:underline${
+            className={`text-black uppercase tracking-widest text-[24px] tablet-md:w-[calc(33.3333%-24px)] cursor-pointer hover:underline${
               activeMenu?.name === item.name ? " activeMenu" : ""
             }`}
             onClick={() => {
