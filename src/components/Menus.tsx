308392a30775a85b707f2ef9d2c14e020723fe81
Scott Wambach <scott@scottwamba.ch>
2025-05-11 14:16:43 -0500

    Update Menus.tsx

--- src/components/Menus.tsx
+++ src/components/Menus.tsx
@@ -11,15 +11,18 @@ export const Menus = ({
     menu: string;
   }[];
 }) => {
-  const [activeMenu, setActiveMenu] = useState<{
-    name: string;
-    menu: string;
-  }>(items[0]);
+  const [activeMenu, setActiveMenu] = useState<
+    | {
+        name: string;
+        menu: string;
+      }
+    | undefined
+  >(items ? items[0] : undefined);
   return (
     <div className="flex flex-col gap-8 py-[100px] px-4 tablet-md:px-8">
       <SectionHeading title="Our Menus" className="text-pangea" element="h2" />
       <div className="flex flex-col tablet-md:flex-row tablet-md:flex-wrap tablet-md:justify-center tablet-md:items-center w-full tablet-md:max-w-[1000px] tablet-md:mx-auto gap-8">
-        {items.map((item) => (
+        {items?.map((item) => (
           <button
             key={item.name}
             className={`text-black uppercase tracking-widest text-[24px] tablet-md:w-[calc(33.3333%-24px)] cursor-pointer hover:underline${
@@ -36,17 +39,19 @@ export const Menus = ({
         ))}
       </div>
 
-      <div className="text-center flex flex-col gap-4 items-center justify-center w-full max-w-[1000px] mx-auto mt-8">
-        <p className="menuTitle text-pangea mobile-lg:px-8 bg-white text-center uppercase font-black tracking-widest text-[26px] relative mb-4">
-          {activeMenu.name}
-        </p>
-        <ButtonLink
-          href={activeMenu.menu}
-          className="text-white px-10 py-2"
-          color="bg-black"
-          text="Download Menu"
-        />
-      </div>
+      {activeMenu && (
+        <div className="text-center flex flex-col gap-4 items-center justify-center w-full max-w-[1000px] mx-auto mt-8">
+          <p className="menuTitle text-pangea mobile-lg:px-8 bg-white text-center uppercase font-black tracking-widest text-[26px] relative mb-4">
+            {activeMenu.name}
+          </p>
+          <ButtonLink
+            href={activeMenu.menu}
+            className="text-white px-10 py-2"
+            color="bg-black"
+            text="Download Menu"
+          />
+        </div>
+      )}
     </div>
   );
 };
