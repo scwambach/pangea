fbaef67ceccc3989d946b67dbbb05b0ec05fb3ff
Scott Wambach <scott@scottwamba.ch>
2025-05-30 10:26:22 -0500

    cleanup

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,11 +1,10 @@
 import { globalSettings } from "./globalSettings";
 import { type SchemaTypeDefinition } from "sanity";
 import { restaurant } from "./restaurant";
-import { tasteMakerLink } from "./tasteMakerLink";
 import { post } from "./post";
 import { category } from "./category";
 import { homePage } from "./homePage";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [globalSettings, homePage, restaurant, tasteMakerLink, post, category],
+  types: [globalSettings, homePage, restaurant, post, category],
 };
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,6 @@
 import { AiOutlineGlobal } from "react-icons/ai";
 import { IoHomeOutline, IoRestaurantOutline } from "react-icons/io5";
 import type { StructureResolver } from "sanity/structure";
-import { FaRegGrinTongue } from "react-icons/fa";
 import { TfiPencilAlt } from "react-icons/tfi";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
@@ -9,22 +8,6 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title("Content")
     .items([
-      S.listItem()
-        .title("Restaurants")
-        .child(S.documentTypeList("restaurant").title("Restaurants"))
-        .icon(IoRestaurantOutline),
-      S.listItem()
-        .title("Posts")
-        .child(
-          S.documentTypeList("post")
-            .title("Posts")
-            .defaultOrdering([{ field: "publishedAt", direction: "asc" }])
-        )
-        .icon(TfiPencilAlt),
-      S.listItem()
-        .title("Media Cards")
-        .child(S.documentTypeList("tasteMakerLink").title("Media Cards"))
-        .icon(FaRegGrinTongue),
       S.listItem()
         .title("Home Page")
         .child(S.document().schemaType("homePage").documentId("homePage"))
@@ -37,4 +20,16 @@ export const structure: StructureResolver = (S) =>
             .documentId("globalSettingss")
         )
         .icon(AiOutlineGlobal),
+      S.listItem()
+        .title("Restaurants")
+        .child(S.documentTypeList("restaurant").title("Restaurants"))
+        .icon(IoRestaurantOutline),
+      S.listItem()
+        .title("Posts")
+        .child(
+          S.documentTypeList("post")
+            .title("Posts")
+            .defaultOrdering([{ field: "publishedAt", direction: "asc" }])
+        )
+        .icon(TfiPencilAlt),
     ]);
