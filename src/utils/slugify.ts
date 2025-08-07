5041630a531f1611f26a433bf29aa20354f427c8
Scott Wambach <scott@scottwamba.ch>
2025-08-07 11:37:54 -0500

    Update slugify.ts

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,4 +1,9 @@
 export const slugify = (text: string) => {
+  // if text is NOT a string, return an empty string
+  if (typeof text !== "string") {
+    return "";
+  }
+
   return text
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, "-")
